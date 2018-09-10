9847a8502b29c61605a05f2846ede435a4d6ac40
Stephen Courtier <steves0089@gmail.com>
2018-09-10 14:26:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     "#{array[0]}"
   elsif array.size == 2 
-    array[0..1.]join(" and ")
+    array[0..1].join(" and ")
   elsif array.size > 3
     array[0...-1].join(", ") << ", and #{array[-1]}"
   end
